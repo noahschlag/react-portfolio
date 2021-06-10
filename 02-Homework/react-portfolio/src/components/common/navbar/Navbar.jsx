a21800f3e971c3e70a7b671c6a8004307faf984d
Noah Schlag <noahschlag12@gmail.com>
2021-06-10 14:52:12 -0500

    changed stying and made page much better looking

--- 02-Homework/react-portfolio/src/components/common/navbar/Navbar.jsx
+++ 02-Homework/react-portfolio/src/components/common/navbar/Navbar.jsx
@@ -5,14 +5,12 @@ import './Navbar.css'
 function Navbar () {
 
     return (
-        <section>
-            <a href="/" className="navbar-item">Home</a>
-            <br></br>
-            <a href="/contact" className="navbar-item">Contact</a>
-            <a href="/About Me" className="navbar-item">About Me</a>
-            <a href="/Photography Portfolio" className="navbar-item">Photography Portfolio</a>
-            <a href="/Code Portfolio" className="navbar-item">Code Portfolio</a>
-        </section>
+        <section className="navbar">
+        <a href="/" className="navbar-item">Home</a>
+        <a href="/about" className="navbar-item">Contact</a>
+        <a href="/portfolio" className="navbar-item">Portfolio</a>
+        <a href="/shop" className="navbar-item">Code portfolio</a>
+    </section>
     )
 
 }
