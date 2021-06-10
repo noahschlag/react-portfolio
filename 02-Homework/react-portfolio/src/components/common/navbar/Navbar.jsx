d883c0ec03ab6d36649c660e3a8357d64978d5ef
Noah Schlag <noahschlag12@gmail.com>
2021-06-10 14:38:24 -0500

    added navbar styling

--- 02-Homework/react-portfolio/src/components/common/navbar/Navbar.jsx
+++ 02-Homework/react-portfolio/src/components/common/navbar/Navbar.jsx
@@ -6,7 +6,12 @@ function Navbar () {
 
     return (
         <section>
-            Navbar
+            <a href="/" className="navbar-item">Home</a>
+            <br></br>
+            <a href="/contact" className="navbar-item">Contact</a>
+            <a href="/About Me" className="navbar-item">About Me</a>
+            <a href="/Photography Portfolio" className="navbar-item">Photography Portfolio</a>
+            <a href="/Code Portfolio" className="navbar-item">Code Portfolio</a>
         </section>
     )
 
